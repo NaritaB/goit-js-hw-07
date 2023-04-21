ba9108e2294a490b302714277e98aa750db1010c
Nataliia Breslavets <breslavets_n@ukr.net>
2023-04-21 22:04:58 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const markup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+galleryContainer.addEventListener('click', onImgClick);
+
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+      `;
+    })
+    .join("");
+}
+
+
+function onImgClick(event) {
+    console.log(event.target);
+}
+
 
 console.log(galleryItems);
